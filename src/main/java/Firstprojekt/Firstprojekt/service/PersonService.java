76631dc94ca6c05453e2b94781afb6b3b4e82332
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2025-07-23 16:09:15 +0200

    service works

--- src/main/java/Firstprojekt/Firstprojekt/service/PersonService.java
+++ src/main/java/Firstprojekt/Firstprojekt/service/PersonService.java
@@ -2,39 +2,20 @@ package Firstprojekt.Firstprojekt.service;
 
 import Firstprojekt.Firstprojekt.model.Person;
 import Firstprojekt.Firstprojekt.repository.PersonRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 
 @Service
 public class PersonService {
 
-    //private List<Person> people = new ArrayList<>();
-    //private int id = 1;
-
     @Autowired
     PersonRepository personRepository;
-
-
-
-
-
-    /*@PostConstruct
-    public void initData(){
-        people.add(new Person(id++, "Ali", 22));
-        people.add(new Person(id++, "Vali", 25));
-        people.add(new Person(id++, "Gulnora", 30));
-    }*/
 
     public List<Person> getPeople(){
         return personRepository.findAll();
@@ -42,8 +23,6 @@ public class PersonService {
 
 
     public boolean addPerson(Person person) {
-        if (personRepository.existsById(person.getId()))  return true;
-
         personRepository.save(person);
         return false;
     }
